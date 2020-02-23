ff72d6a5d2235ffcbf563a6b90bcd8df1d7ef310
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-02-24 00:52:28 +0530

    Implementing @Before advice

--- 1-Web-Customer-Tracker/src/com/sarthak/springdemo/aspect/CRMLoggingAspect.java
+++ 1-Web-Customer-Tracker/src/com/sarthak/springdemo/aspect/CRMLoggingAspect.java
@@ -1,6 +1,8 @@
 package com.sarthak.springdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.jboss.logging.Logger;
 import org.springframework.stereotype.Component;
@@ -13,16 +15,16 @@ public class CRMLoggingAspect {
 	private Logger myLogger = Logger.getLogger(getClass().getName());
 
 	// set up pointcut declaration
-	@Pointcut("execution(* com.sarthak.springdemo.controller.*.*.(..))")
+	@Pointcut("execution(* com.sarthak.springdemo.controller.*.*(..))")
 	private void forControllerPackage() {
 	}
 
 	// do the same for servuce package and dao package
-	@Pointcut("execution(* com.sarthak.springdemo.service.*.*.(..))")
+	@Pointcut("execution(* com.sarthak.springdemo.service.*.*(..))")
 	private void forServicePackage() {
 	}
 
-	@Pointcut("execution(* com.sarthak.springdemo.dao.*.*.(..))")
+	@Pointcut("execution(* com.sarthak.springdemo.dao.*.*(..))")
 	private void forDaoPackage() {
 	}
 
@@ -32,6 +34,18 @@ public class CRMLoggingAspect {
 	}
 
 	// add @Before advice
+
+	@Before("forAppFlow()")
+	public void before(JoinPoint theJoinPoint) {
+
+		// display method we are calling
+		String method = theJoinPoint.getSignature().toShortString();
+		myLogger.info("======> in @Before: calling method: " + method);
+
+		// display the arguments to the method
+		
+
+	}
 
 	// add @AfterReturning advice
 
