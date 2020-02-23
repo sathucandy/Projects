79373d21d4fccc0063c8ef5594bdc171a5ac28a6
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-02-24 00:38:00 +0530

    Implementing pointcut expressions

--- 1-Web-Customer-Tracker/src/com/sarthak/springdemo/aspect/CRMLoggingAspect.java
+++ 1-Web-Customer-Tracker/src/com/sarthak/springdemo/aspect/CRMLoggingAspect.java
@@ -1,6 +1,7 @@
 package com.sarthak.springdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.jboss.logging.Logger;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,23 @@ public class CRMLoggingAspect {
 	private Logger myLogger = Logger.getLogger(getClass().getName());
 
 	// set up pointcut declaration
+	@Pointcut("execution(* com.sarthak.springdemo.controller.*.*.(..))")
+	private void forControllerPackage() {
+	}
+
+	// do the same for servuce package and dao package
+	@Pointcut("execution(* com.sarthak.springdemo.service.*.*.(..))")
+	private void forServicePackage() {
+	}
+
+	@Pointcut("execution(* com.sarthak.springdemo.dao.*.*.(..))")
+	private void forDaoPackage() {
+	}
+
+	@Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+	private void forAppFlow() {
+
+	}
 
 	// add @Before advice
 
