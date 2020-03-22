3df714bb4916beb4524d378f97ff9507ec90419a
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-03-22 21:27:33 +0530

    Getting customer by id

--- 4-spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ 4-spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,13 @@ public class CustomerRestController {
 	@GetMapping("/customers")
 	public List<Customer> getCustomer() {
 		return customerService.getCustomers();
+	}
+
+	// add mapping for GET /customer/{customerId}
+	@GetMapping("/customers/{customerId}")
+	public Customer getCustomer(@PathVariable int customerId) {
+		Customer theCustomer = customerService.getCustomer(customerId);
+		return theCustomer;
 	}
 
 }
