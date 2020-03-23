b053dffd6690aaa4ec5b2d732caf5be29180d0f3
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-03-23 16:42:00 +0530

    Sending back error

--- 4-spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerRestExceptionHandler.java
+++ 4-spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerRestExceptionHandler.java
@@ -1,5 +1,28 @@
 package com.luv2code.springdemo.rest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class CustomerRestExceptionHandler {
+
+	// Add an exception for CustomerNotFoundException
+
+	@ExceptionHandler
+	public ResponseEntity<CustomerErrorResponse> handleException(CustomerNotFoundException exe) {
+
+		// create CustomerErrorResponse
+		CustomerErrorResponse error = new CustomerErrorResponse(HttpStatus.NOT_FOUND.value(), exe.getMessage(),
+				System.currentTimeMillis());
+
+		// return response Entity
+
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+
+	}
+
+	// Add Another Exception handler ... to catch any exception. (catch alll)
 
 }
