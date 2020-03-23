099b8b1cb6627b30cbe93c8c58e159d682f12dc3
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-03-23 20:45:36 +0530

    Updating Customers

--- 4-spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ 4-spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,6 +46,16 @@ public class CustomerRestController {
 		// this is force a save of new item .... instead of update
 
 		theCustomer.setId(0);
+
+		customerService.saveCustomer(theCustomer);
+
+		return theCustomer;
+	}
+
+	// add mapping for PUT /customers - update existing customer
+
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer theCustomer) {
 
 		customerService.saveCustomer(theCustomer);
 
