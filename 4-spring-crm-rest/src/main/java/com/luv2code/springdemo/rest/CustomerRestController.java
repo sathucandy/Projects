935200b91da72ed37e3af9d7b420f3555beaf03d
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-03-23 16:32:22 +0530

    Creating responses to handle exception

--- 4-spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ 4-spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -29,6 +29,11 @@ public class CustomerRestController {
 	@GetMapping("/customers/{customerId}")
 	public Customer getCustomer(@PathVariable int customerId) {
 		Customer theCustomer = customerService.getCustomer(customerId);
+
+		if (theCustomer == null) {
+			throw new CustomerNotFoundException("Customer id not found: " + customerId);
+		}
+
 		return theCustomer;
 	}
 
