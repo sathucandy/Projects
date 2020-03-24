1c14785ddb0e16557a457f825d4843895f708588
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-03-24 14:02:17 +0530

    Generating category component

--- 3-EcommerceProject/frontend/angular-ecommerce/src/app/app.module.ts
+++ 3-EcommerceProject/frontend/angular-ecommerce/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HttpClientModule } from "@angular/common/http";
 import { ProductService } from "./services/product.service";
 
 import { Routes, RouterModule } from "@angular/router";
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 
 const routes: Routes = [
   { path: "category/:id", component: ProductListComponent },
@@ -18,7 +19,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent],
+  declarations: [AppComponent, ProductListComponent, ProductCategoryMenuComponent],
   imports: [RouterModule.forRoot(routes), BrowserModule, HttpClientModule],
   providers: [ProductService],
   bootstrap: [AppComponent]
