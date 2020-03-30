53d562ec72baddf79d84cd084bd44640b0e9dac4
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-03-30 14:14:23 +0530

    Updating employee

--- 5-CrudDemo/src/main/java/com/sarthak/cruddemo/rest/EmployeeRestController.java
+++ 5-CrudDemo/src/main/java/com/sarthak/cruddemo/rest/EmployeeRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,6 +49,14 @@ public class EmployeeRestController {
 		// also just in case they pass an id in json ... set it to 0
 		// this is to force a save of new item ... instead of update
 		theEmployee.setId(0);
+		employeeService.save(theEmployee);
+		return theEmployee;
+	}
+
+	// add mapping for PUT /employee - update existing employee
+
+	@PutMapping("/employees")
+	public Employee updateEmployee(@RequestBody Employee theEmployee) {
 		employeeService.save(theEmployee);
 		return theEmployee;
 	}
