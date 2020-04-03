44dbe2a10f35c855c347728421fb8db89b4f0919
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-04-03 18:53:52 +0530

    Updating pom

--- 8-CrudDemo-spring-data-rest-jpa/src/main/java/com/sarthak/cruddemo/dao/EmployeeRepository.java
+++ 8-CrudDemo-spring-data-rest-jpa/src/main/java/com/sarthak/cruddemo/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.sarthak.cruddemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.sarthak.cruddemo.entity.Employee;
 
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 	
 	// thats it no need to write code
