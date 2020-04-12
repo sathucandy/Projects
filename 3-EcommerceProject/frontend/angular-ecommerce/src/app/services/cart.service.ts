e7ee94481feaa9c14b92337fab5c980216a8943f
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-04-12 16:37:04 +0530

    Computing total in cart service

--- 3-EcommerceProject/frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ 3-EcommerceProject/frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -40,6 +40,32 @@ export class CartService {
     this.computeCartTotals();
   }
   computeCartTotals() {
-    throw new Error("Method not implemented.");
+    let totalPriceValue: number = 0;
+    let totalQuantityValue: number = 0;
+    for (let currentCartItem of this.cartItems) {
+      totalPriceValue += currentCartItem.quantity * currentCartItem.unitPrice;
+      totalQuantityValue += currentCartItem.quantity;
+    }
+    // publish the new values .. all subscribers will recieve the new data
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuantity.next(totalQuantityValue);
+
+    // log the cart data for debugging purposes
+    this.logCartData(totalPriceValue, totalQuantityValue);
+  }
+  logCartData(totalPriceValue: number, totalQuantityValue: number) {
+    console.log("Contents of cart");
+    for (let tempCartItem of this.cartItems) {
+      const subTotalPrice = tempCartItem.quantity * tempCartItem.unitPrice;
+      console.log(
+        `name: ${tempCartItem.name}, quantity: ${tempCartItem.quantity}, unitPrice: ${tempCartItem.unitPrice}, subTotalPrice: ${subTotalPrice}`
+      );
+    }
+    console.log(
+      `totalPrice: ${totalPriceValue.toFixed(
+        2
+      )}, totalQuantity: ${totalQuantityValue}`
+    );
+    console.log("--------------------");
   }
 }
