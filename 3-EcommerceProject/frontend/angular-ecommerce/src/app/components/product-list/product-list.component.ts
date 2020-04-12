746e0fd08280c442da047ef7ba16dd6b4d474d02
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-04-12 17:16:16 +0530

    subscribing to cart service

--- 3-EcommerceProject/frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 3-EcommerceProject/frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { ProductService } from "src/app/services/product.service";
 import { Product } from "src/app/common/product";
 import { ActivatedRoute } from "@angular/router";
+import { CartItem } from "src/app/common/cart-item";
+import { CartService } from "src/app/services/cart.service";
 
 @Component({
   selector: "app-product-list",
@@ -25,6 +27,7 @@ export class ProductListComponent implements OnInit {
 
   constructor(
     private productService: ProductService,
+    private cartService: CartService,
     private route: ActivatedRoute
   ) {}
 
@@ -125,5 +128,7 @@ export class ProductListComponent implements OnInit {
       `Adding to cart:  + ${theProduct.name}, ${theProduct.unitPrice}`
     );
     // TODO.. do the real work
+    const theCartItem = new CartItem(theProduct);
+    this.cartService.addToCart(theCartItem);
   }
 }
